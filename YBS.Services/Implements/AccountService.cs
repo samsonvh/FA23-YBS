c90150a5617b28f73abaa093ed76bb1e575383ba
phamquangthang119 <phamquangthang119@gmail.com>
2023-09-30 14:55:36 +0700

    Add genericRepository, IGenericRepository

---
+++ YBS.Services/Implements/AccountService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YBS.Data.Models;
+using YBS.Data.Repositories.Interfaces;
+using YBS.Dtos;
+using YBS.Services.Interfaces;
+
+namespace YBS.Services.Implements
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IGenericRepository<Account> _accountRepository;
+        public AccountService(IGenericRepository<Account> accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+      /*  public Task<AccountDto> GetById(int id)
+        {
+
+        }*/
+    }
+}
