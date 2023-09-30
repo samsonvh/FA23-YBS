779794b3ce7d3af3e2eb6458957cea89dfe770e8
phamquangthang119 <phamquangthang119@gmail.com>
2023-09-30 13:02:58 +0700

    Stay folder and class

---
+++ YBS.Data/Models/Route.cs
@@ -0,0 +1,21 @@
+﻿namespace YBS.Data.Models
+{
+    public class Route
+    {
+        public int Id { get; set; } 
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+        public string Name { get; set; }    
+        public string Beginning { get; set; }
+        public string Destination { get; set; }
+        public TimeSpan PickupTime { get; set; }
+        public TimeSpan StartingTime { get; set; }
+        public TimeSpan EndingTime { get; set; }
+        public int DurationTime { get; set; }
+        public string DurationUnit { get; set; }
+        public float Price { get; set; }
+        public string Unit { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+    }
+}
