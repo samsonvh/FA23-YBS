bba02bfaf959b5f8675ab2bc8eb1055efdc5c2d0
KhangNNDev <Khanga123456789@gmail.com>
2023-10-04 04:59:28 +0700

    update model (cont)

--- YBS/Controllers/MemberController.cs
+++ YBS/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using YBS.Services.DataHandler.Requests.MemberRequests;
 using YBS.Services.Services.Interfaces;
 
 namespace YBS.Controllers
@@ -12,10 +13,11 @@ namespace YBS.Controllers
         {
             _memberService = memberService;
         }
-        // [HttpPost("GoogleLogin")]
-        // public async Task<IActionResult> Create(string idToken)
-        // {
-        //     return Ok(await _memberService.LoginWithGoogle (idToken));
-        // }
+        [HttpPost]
+        public async Task<IActionResult> Create(MemberCreateRequest request)
+        {
+            await _memberService.Create(request);
+            return Ok();
+        }
     }
 }
