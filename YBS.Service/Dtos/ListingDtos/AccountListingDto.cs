96408f83a780d9fbcb459f62a69e77e301b69a89
phamquangthang119 <phamquangthang119@gmail.com>
2023-10-09 13:39:30 +0700

    Add AccountListingDto, DefaultPageRequest, DefaultPageResponse, YBS.Service.csproj, Program.cs

--- YBS.Service/Dtos/ListingDtos/AccountListingDto.cs
+++ YBS.Service/Dtos/ListingDtos/AccountListingDto.cs
@@ -7,6 +7,10 @@ namespace YBS.Service.Dtos.ListingDtos
 {
     public class AccountListingDto
     {
-        
+        public int Id { get; set; }
+        public string Role { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
     }
 }
--- YBS/Program.cs
+++ YBS/Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using YBS.Data.Context;
+using YBS.Data.UnitOfWorks;
+using YBS.Data.UnitOfWorks.Implements;
+using YBS.Service.Services;
+using YBS.Service.Services.Implements;
+using YBS.Service.Utils.AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<YBSContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("YBSContext")));
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+builder.Services.AddScoped<IUnitOfWorks, UnitOfWorks>();
+builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
