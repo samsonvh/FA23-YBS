ee5f9c73f61d07382bc09a2ad49e6d49c4af710a
KhangNNDev <Khanga123456789@gmail.com>
2023-10-11 14:27:40 +0700

    remove YBS.Services

--- YBS.Services/Dtos/PageRequestDtos/AccountGetAllRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace YBS.Services.Dtos.Requests
-{
-    public class AccountGetAllRequest : PageRequest
-    {
-        public string? PhoneNumber { get; set; }
-        public string? Email { get; set; }
-    }
-}
--- YBS.Services/Dtos/PageRequestDtos/MemberGetAllRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace YBS.Services.Dtos.Requests
-{
-    public class MemberGetAllRequest : PageRequest
-    { 
-        public string? FullName { get; set; }
-        public string? Email { get; set; }
-        public string? PhoneNumber { get; set; }
-    }
-}
