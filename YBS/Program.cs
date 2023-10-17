bb297dee9e2d07ebba412828999f988dc19df2a0
phamquangthang119 <phamquangthang119@gmail.com>
2023-10-17 14:33:44 +0700

    Update cors

--- YBS/Program.cs
+++ YBS/Program.cs
@@ -83,7 +83,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(MyAllowSpecificOrigins, builder =>
     {
-        builder.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+        builder.WithOrigins("http://localhost:3000", "http://localhost:3001").AllowAnyMethod().AllowAnyHeader();
     });
 });
 
