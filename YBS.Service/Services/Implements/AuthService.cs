2c27be67545f38c8714288c54eafe2e889696d9e
phamquangthang119 <phamquangthang119@gmail.com>
2023-11-06 14:25:04 +0700

    Uncomment authentication

--- YBS.Service/Services/Implements/AuthService.cs
+++ YBS.Service/Services/Implements/AuthService.cs
@@ -1,4 +1,4 @@
-/*using System;
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
@@ -321,4 +321,4 @@ namespace YBS.Service.Services.Implements
             return token;
         }
     }
-}*/
+}
--- YBS/Middlewares/RoleAuthorizationAttribute.cs
+++ YBS/Middlewares/RoleAuthorizationAttribute.cs
@@ -1,4 +1,4 @@
-/*using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,4 +13,4 @@ namespace YBS.Middlewares
             Arguments = new object[] { role };
         }
     }
-}*/
+}
