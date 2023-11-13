9bb6f4fc5a07158ae88fb062e9d4a1750836339a
phamquangthang119 <phamquangthang119@gmail.com>
2023-11-13 15:17:01 +0700

    Remove money unit service

--- YBS.Service/Dtos/ListingDtos/ServiceListingDto.cs
+++ YBS.Service/Dtos/ListingDtos/ServiceListingDto.cs
@@ -13,7 +13,6 @@ namespace YBS.Service.Dtos.ListingDtos
         public string Type { get; set; }
         public string Name { get; set; }    
         public string? Description { get; set; }    
-        public string MoneyUnit { get; set; }
         public string Status { get; set; }
     }
 }
